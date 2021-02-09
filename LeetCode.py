e185de611eb2ceb52943380155c72b69a2835748
chamin <chamin.lokugamhewage@ucdconnect.ie>
2021-02-09 12:45:47 +0000

    Reverse signed interger

--- LeetCode.py
+++ LeetCode.py
@@ -11,3 +11,23 @@ class Solution:
                 break
 
         return output
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1
+        if (x > 0):
+            sign = 1
+        elif (x < 0):
+            sign = -1
+
+        number = str(abs(x))
+        charArr = list(number)
+        reversedNum = int(''.join(reversed(charArr)))
+
+        numToReturn = int(reversedNum) * sign
+
+        if numToReturn not in range((-1 << 31), (1 << 31) - 1):
+            return 0
+        else:
+            return numToReturn
